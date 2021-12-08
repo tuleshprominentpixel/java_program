9eea7d14b92f208035d5d59711f989c3c3dd63c1
tuleshprominentpixel <tulesh.g@prominentpixel.com>
2021-12-08 11:06:39 +0530

    08-12

---
+++ my personal/shubhashsir/2_withoutcomment/src/com/company/Main.java
@@ -0,0 +1,125 @@
+package com.company;
+
+import java.util.*;
+
+class Student {
+    private String studentName;
+    private String collegeName;
+
+    Student(String studentName, String collegeName) {
+        this.studentName = studentName;
+        this.collegeName = collegeName;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "studentName='" + studentName + '\'' +
+                ", collegeName='" + collegeName + '\'' +
+                '}';
+    }
+
+}
+
+public class Main {
+    //List
+    static List < Student > listOfStudent = new ArrayList();
+    //Hashmap
+    static HashMap < String, String > studentOfCollegeHashmap = new HashMap < > ();
+    static HashMap < String, Integer > stuentCountHashmap = new HashMap < > ();
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Student s1 = new Student("a1", "modi");
+        Student s2 = new Student("a2", "modi");
+        Student s3 = new Student("a3", "modi");
+        Student s4 = new Student("a4", "modi");
+        Student s5 = new Student("a1", "atmiya");
+        Student s6 = new Student("b2", "atmiya");
+        Student s7 = new Student("b3", "atmiya");
+        Student s8 = new Student("b4", "harivadana");
+        Student s9 = new Student("c1", "harivadana");
+        Student s10 = new Student("c2", "harivadana");
+        Student s11 = new Student("c3", "harivadana");
+        Student s12 = new Student("c3", "krishna");
+        Student s13 = new Student("c3", "krishna");
+        Student s14 = new Student("c3", "krishna");
+        Student s15 = new Student("c6", "krishna");
+        Student s16 = new Student("c7", "krishna");
+
+        listOfStudent.add(s1);
+        listOfStudent.add(s2);
+        listOfStudent.add(s3);
+        listOfStudent.add(s4);
+        listOfStudent.add(s5);
+        listOfStudent.add(s6);
+        listOfStudent.add(s7);
+        listOfStudent.add(s8);
+        listOfStudent.add(s9);
+        listOfStudent.add(s10);
+        listOfStudent.add(s11);
+        listOfStudent.add(s12);
+        listOfStudent.add(s13);
+        listOfStudent.add(s14);
+        listOfStudent.add(s15);
+        listOfStudent.add(s16);
+
+        System.out.println(getStudentsOfCollege(listOfStudent));
+        System.out.println("---------------------------------");
+        System.out.println(getStudentCount(listOfStudent));
+    }
+    //excecise -1
+    public static HashMap< String, Integer > getStudentCount(List<Student> studentList) {
+
+        for(Student s1:studentList){
+            if ((!stuentCountHashmap.containsKey(s1.getCollegeName()))) {
+                stuentCountHashmap.put(s1.getCollegeName(), 1);
+            } else {
+                stuentCountHashmap.put(s1.getCollegeName(), stuentCountHashmap.get(s1.getCollegeName()) + 1);
+            }
+        }
+        return stuentCountHashmap;
+    }
+    //excecise -2
+    public static HashMap< String, String > getStudentsOfCollege(List<Student> studentList) {
+        /*for (int i = 0; i < studentList.stream().count(); i++) {
+            if ((!studentOfCollegeHashmap.containsKey(studentList.get(i).getCollegeName()))) {
+                studentOfCollegeHashmap.put(studentList.get(i).getCollegeName(), studentList.get(i).getStudentName());
+
+            } else {
+                var tempStoreStudentName = studentOfCollegeHashmap.get(studentList.get(i).getCollegeName());
+                tempStoreStudentName += " , " + studentList.get(i).getStudentName();
+                studentOfCollegeHashmap.put(studentList.get(i).getCollegeName(), tempStoreStudentName);
+            }
+        }*/
+        for(Student studList:studentList)
+        {
+            if ((!studentOfCollegeHashmap.containsKey(studList.getCollegeName()))) {
+                studentOfCollegeHashmap.put(studList.getCollegeName(), studList.getStudentName());
+
+            } else {
+                var tempStoreStudentName = studentOfCollegeHashmap.get(studList.getCollegeName());
+                tempStoreStudentName += " , " + studList.getStudentName();
+                studentOfCollegeHashmap.put(studList.getCollegeName(), tempStoreStudentName);
+            }
+        }
+        return studentOfCollegeHashmap;
+    }
+}
